7a3493645db99401de71ee6ef971b1f9a35cb49e
Lane Seppala <lseppala@users.noreply.github.com>
2025-05-28 09:52:03 -0600

    Fix autoload

--- plugin/praise.lua
+++ plugin/praise.lua
@@ -4,6 +4,6 @@ end
 vim.g.loaded_praise = true
 
 -- Create command directly without requiring setup
-  vim.api.nvim_create_user_command('Praise', function()
-    M.find_pr()
-  end, { desc = 'Find and open PR the PR that introduced or changed code on the current line' })
+vim.api.nvim_create_user_command('Praise', function()
+	require('praise.nvim').find_pr()
+end, { desc = 'Find and open PR the PR that introduced or changed code on the current line' })
